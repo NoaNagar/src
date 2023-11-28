1d9daf523bf80fbf0d859bfaed61be5621c3f327
NoaNagar <BENAG1972@GMAIL.COM>
2023-11-28 23:47:15 +0200

    2

--- App.jsx
+++ App.jsx
@@ -4,73 +4,31 @@ import { ToastContainer } from "react-toastify";
 import Router from "./routes/Router";
 import useAutoLogin from "./hooks/useAutoLogin";
 import { Fragment, useEffect, useState } from "react";
-import { LinearProgress, ThemeProvider, createTheme, css } from "@mui/material";
-import { BarLoader } from "react-spinners";
-import CircularProgress from "@mui/material/CircularProgress";
-
-const override = css`
-  display: block;
-  margin: 0 auto;
-  border-color: red;
-  margin-top: 20vh;
-`;
+import { LinearProgress } from "@mui/material";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
   const [doneAuth, setDoneAuth] = useState(false);
   const autoLogin = useAutoLogin();
-
   useEffect(() => {
     (async () => {
       try {
-        await autoLogin();
+        await autoLogin(); //false is default
       } catch (err) {
         console.log(err);
       } finally {
+        //this block of code will executed when the promise done
+        //no matter if its done or got error
         setDoneAuth(true);
       }
     })();
-  }, [autoLogin]);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       await new Promise((resolve) => setTimeout(resolve));
-  //       setLoading(false);
-  //     } catch (err) {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [autoLogin]);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       await autoLogin();
-  //     } catch (err) {
-  //     } finally {
-  //       setDoneAuth(true);
-  //     }
-  //   })();
-  // }, []);
+  }, []);
 
   return (
     <Fragment>
-      {/* {loading ? (
-        // <CircularProgress sx={{ mt: 12, ml: "auto", mr: "auto" }} />
-        <BarLoader
-          css={override}
-          size={150}
-          color={"#123abc"}
-          loading={loading}
-        />
-      ) : ( */}
       <LayoutComponent>
         <ToastContainer />
         {doneAuth ? <Router /> : <LinearProgress sx={{ mt: 15 }} />}
       </LayoutComponent>
-      {/* )} */}
     </Fragment>
   );
 };
